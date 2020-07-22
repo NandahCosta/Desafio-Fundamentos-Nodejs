dce6e6eb6d0b1475c7a50dcce0b54760c781683e
NandahCosta <fernanda.aquinno@outlook.com>
2020-07-22 22:50:55 +0100

    :sparkles: Done - Transactions-Income/outcome

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request{
+  title: string;
+  value:  number;
+  type: 'income' |'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,25 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({title, value, type}:Request): Transaction {
+    // TODO -- validar se o valor inserido é valido - segurança
+    if(!["income", "outcome"].includes(type)){
+      throw new Error ('Infelizmente este tipo de transação não é permitida');
+    }
+
+    const {total} = this.transactionsRepository.getBalance();
+
+    if(type === "outcome" && total < value){
+      throw new Error ('Dim Dim Cabou :(')
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
